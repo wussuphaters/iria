4b2d2af01e7eb4f4e8099719872cc89c5ae4b4d0
maelponchant <mael.ponchant@outlook.com>
2020-05-10 17:41:40 +0200

    Deleted usercard class and placed content directly in listview builder so that list can be updated if a user is modified

--- lib/pages/UsersScreen.dart
+++ lib/pages/UsersScreen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iria/Menu.dart';
 import 'package:iria/Routes.dart';
-import 'package:iria/UserCard.dart';
 import 'package:iria/objects/API.dart';
 import 'package:iria/objects/User.dart';
 
@@ -39,7 +38,36 @@ class _UsersScreenState extends State<UsersScreen> {
           body: snapshot.hasData ? ListView.builder(
             itemCount: users.length,
             itemBuilder: (BuildContext context, int index)  {
-              return UserCard(user: users[index], api: widget.api);
+              User user = User.fromJson(users[index]);
+              return user != null ? ExpansionTile(
+                title: Row(
+                  children: <Widget>[
+                    Icon(user.isAdmin ? Icons.verified_user : Icons.account_circle),
+                    Text("${user.firstName} ${user.lastName}")
+                  ]
+                ),
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Row(children: <Widget>[Icon(Icons.email), Text(user.email)]),
+                      Row(children: <Widget>[Icon(Icons.phone), Text(user.phoneNumber)]),
+                      user.expiration != null ? Row(children: <Widget>[Icon(Icons.access_time), Text(user.expiration.toString())]) : SizedBox.shrink(),
+                      Row(children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.mode_edit),
+                          onPressed: () {
+                            Navigator.pushNamed(context, Routes.updateUser, arguments: {'user': user}).then((value) {setState(() {});});
+                          }
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () => handleDelete(context)
+                        )
+                      ])
+                    ],
+                  )
+                ]
+              ) : SizedBox.shrink();
             },
           ) : Center(
             child: FutureBuilder(
@@ -58,6 +86,38 @@ class _UsersScreenState extends State<UsersScreen> {
           ),
           drawer: Menu(user: user),
         );
+      },
+    );
+  }
+
+  void handleDelete(BuildContext context)  {
+    AlertDialog deleteUserDialog = AlertDialog(
+      title: Text("Supprimer l'utilisateur"),
+      content: Text("Êtes vous sûr de vouloir supprimer ${user.firstName} des utilisateurs ?"),
+      actions: <Widget>[
+        FlatButton(
+          child: Text("ANNULER"),
+          onPressed: (){
+            Navigator.pop(context);
+          }
+        ),
+        FlatButton(
+          child: Text("SUPPRIMER"),
+          onPressed: () {
+            widget.api.deleteUser(user.id);
+            setState(() {
+              user = null;
+            });
+            Navigator.pop(context);
+          }
+        )
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return deleteUserDialog;
       },
     );
   }
